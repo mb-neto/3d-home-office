b8b9dea905cd855770c134ac58e095eccbd770fe
Manoel Benedito Neto <manoelneto164@gmail.com>
2021-04-23 22:45:20 -0300

    Manemariallyson homeoffice 1.3 pc

--- sala-de-aula/src/computer.cpp
+++ sala-de-aula/src/computer.cpp
@@ -12,91 +12,91 @@ void Computer :: drawComputer() {
 	//glRotatef(10, 1.0f, 0.0f, 0.0f);
 	//glRotatef(-10, 0.0f, 0.0f, 1.0f);
 	//glRotatef(_angle, 0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 0.0f);
+	glColor3f(0.0, 0.0, 0.0);
 	glBegin(GL_QUADS);
 
 	//Front
 	//
-	glVertex3f(-2.0f, -0.2f, 2.0f);
-	glVertex3f(2.0f, -0.2f, 2.0f);
-	glVertex3f(2.0f, 0.2f, 2.0f);
-	glVertex3f(-2.0f, 0.2f, 2.0f);
+	glVertex3f(-1.8f, -0.2f, 2.0f);
+	glVertex3f(1.8f, -0.2f, 2.0f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
 
 	//Right
 	//
-	glVertex3f(2.0f, -0.2f, -2.0f);
-	glVertex3f(2.0f, 0.2f, -2.0f);
-	glVertex3f(2.0f, 0.2f, 2.0f);
-	glVertex3f(2.0f, -0.2f, 2.0f);
+	glVertex3f(1.8f, -0.2f, -1.0f);
+	glVertex3f(1.8f, 0.0f, -1.0f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, -0.2f, 2.0f);
 
 	//Back
 	//;
-	glVertex3f(-2.0f, -0.2f, -2.0f);
-	glVertex3f(-2.0f, 0.2f, -2.0f);
-	glVertex3f(2.0f, 0.2f, -2.0f);
-	glVertex3f(2.0f, -0.2f, -2.0f);
+	glVertex3f(-1.8f, -0.2f, -1.0f);
+	glVertex3f(-1.8f, 0.0f, -1.0f);
+	glVertex3f(1.8f, 0.0f, -1.0f);
+	glVertex3f(1.8f, -0.2f, -1.0f);
 
 	//Left
 	;
-	glVertex3f(-2.0f, -0.2f, -2.0f);
-	glVertex3f(-2.0f, -0.2f, 2.0f);
-	glVertex3f(-2.0f, 0.2f, 2.0f);
-	glVertex3f(-2.0f, 0.2f, -2.0f);
+	glVertex3f(-1.8f, -0.2f, -1.0f);
+	glVertex3f(-1.8f, -0.2f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, -1.0f);
 
 	//top
 	//
 
-	glVertex3f(2.0f, 0.2f, 2.0f);
-	glVertex3f(-2.0f, 0.2f, 2.0f);
-	glVertex3f(-2.0f, 0.2f, -2.0f);
-	glVertex3f(2.0f, 0.2f, -2.0f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, -1.0f);
+	glVertex3f(1.8f, 0.0f, -1.0f);
 
 	//bottom
 	;
 
-	glVertex3f(2.0f, -0.2f, 2.0f);
-	glVertex3f(-2.0f, -0.2f, 2.0f);
-	glVertex3f(-2.0f, -0.2f, -2.0f);
-	glVertex3f(2.0f, -0.2f, -2.0f);
+	glVertex3f(1.8f, -0.2f, 2.0f);
+	glVertex3f(-1.8f, -0.2f, 2.0f);
+	glVertex3f(-1.8f, -0.2f, -1.0f);
+	glVertex3f(1.8f, -0.2f, -1.0f);
 
-	//chair upper part 
+	//tela upper part 
 	glColor3f(0.5, 0.5, 0.5);
 
     // frente e tras
 	
-	glVertex3f(-1.8f, 0.2f, 1.8f);
-	glVertex3f(1.8f, 0.2f, 1.8f);
-	glVertex3f(1.8f, 2.5f, 1.8f);
-	glVertex3f(-1.8f, 2.5f, 1.8f);
+	glVertex3f(-1.8f, 0.0f, 1.95f);
+	glVertex3f(1.8f, 0.0f, 1.95f);
+	glVertex3f(1.8f, 2.3f, 1.95f);
+	glVertex3f(-1.8f, 2.3f, 1.95f);
 
-	glVertex3f(-1.8f, 0.2f, 2.0f);
-	glVertex3f(1.8f, 0.2f, 2.0f);
-	glVertex3f(1.8f, 2.5f, 2.0f);
-	glVertex3f(-1.8f, 2.5f, 2.0f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, 2.3f, 2.0f);
+	glVertex3f(-1.8f, 2.3f, 2.0f);
 
 	//chair upper lados
 	glColor3f(0.5, 0.5, 0.5);
-	glVertex3f(-1.8f, 0.2f, 1.8f);
-	glVertex3f(-1.8f, 0.2f, 2.0f);
-	glVertex3f(-1.8f, 2.5f, 2.0f);
-	glVertex3f(-1.8f, 2.5f, 1.8f);
+	glVertex3f(-1.8f, 0.0f, 1.95f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
+	glVertex3f(-1.8f, 2.3f, 2.0f);
+	glVertex3f(-1.8f, 2.3f, 1.95f);
 
-	glVertex3f(1.8f, 0.2f, 1.80f);
-	glVertex3f(1.8f, 0.2f, 2.0f);
-	glVertex3f(1.8f, 2.5f, 2.0f);
-	glVertex3f(1.8f, 2.5f, 1.8f);
+	glVertex3f(1.8f, 0.0f, 1.95f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, 2.3f, 2.0f);
+	glVertex3f(1.8f, 2.3f, 1.95f);
 
 	//chair upper cima e baixo
 	glColor3f(0.5, 0.5, 0.5);
-	glVertex3f(-1.8f, 2.5f, 1.80f);
-	glVertex3f(-1.8f, 2.5f, 2.0f);
-	glVertex3f(1.8f, 2.5f, 2.0f);
-	glVertex3f(1.8f, 2.5f, 1.8f);
+	glVertex3f(-1.8f, 2.3f, 1.95f);
+	glVertex3f(-1.8f, 2.3f, 2.0f);
+	glVertex3f(1.8f, 2.3f, 2.0f);
+	glVertex3f(1.8f, 2.3f, 1.95f);
 
-	glVertex3f(-1.8f, 0.2f, 1.80f);
-	glVertex3f(-1.8f, 0.2f, 2.0f);
-	glVertex3f(1.8f, 0.2f, 2.0f);
-	glVertex3f(1.8f, 0.2f, 1.8f);
+	glVertex3f(-1.8f, 0.0f, 1.95f);
+	glVertex3f(-1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, 0.0f, 2.0f);
+	glVertex3f(1.8f, 0.0f, 1.95f);
 
 	//glVertex3f()
 
